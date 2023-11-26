20c56f9dacb97b76b503bbd523ece3bec34e0d07
Gerardo Napoli <gnapoli@florence-consulting.it>
2023-11-26 01:21:18 +0100

    Project Structure

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.dropTheNumber import DropTheNumber
+
+if __name__ == "__main__":
+    DropTheNumber.start()
---
+++ src/dropTheNumber.py
@@ -0,0 +1,7 @@
+import sys
+import pygame
+from state import Home, Game
+
+class DropTheNumber:
+    def start():
+        print("Hello world")
